e85af44b4951c82a4275f2b57732df03d26a8254
Kajal Kumari <kajalkumari979834@gmail.com>
2026-09-08 22:14:05 +0530

    Add files via upload

---
+++ LIST/MERGE/compress plus.py
@@ -0,0 +1,4 @@
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+res = [x for x in list1]+[x for x in list2]
+print(res)
---
+++ LIST/MERGE/plus.py
@@ -0,0 +1,4 @@
+list1 = [1, 2, 3 , 4]
+list2 = [4, 5, 6]
+res = list1 + list2
+print(set(res))
