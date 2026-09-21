9d10d6a80f6984dac9b653742fae20116c508c7c
Kajal Kumari <kajalkumari979834@gmail.com>
2026-09-21 14:47:31 +0530

    Add files via upload

---
+++ DICT/DELETE/pop/pop no error.py
@@ -0,0 +1,4 @@
+d = {"name": "Kajal", "age": 22, "city": "Bhopal"}
+d.pop("name")
+d.pop("name" , "Not Found")
+print(d)
---
+++ DICT/DELETE/pop/popitems.py
@@ -0,0 +1,4 @@
+d = {"name": "Kajal", "age": 22, "city": "Bhopal"}
+res = d.popitem()
+print(res)
+print(d)
